179f0da5d7ea3e8fcfa40d9aeccba63d134c905a
Magdiel <magdielgomesferreira@gmail.com>
2025-10-30 11:09:08 -0300

    Página de Contato

---
+++ jsgeral/script.js
@@ -0,0 +1,19 @@
+document.getElementById("botaolista").addEventListener("click", show);
+
+function show(){
+    let listanav = document.getElementById("listanav").style.display;
+    if(listanav === "none")
+    {
+        document.getElementById("listanav").style.display = "block";
+    }
+    else{
+        document.getElementById("listanav").style.display = "none";
+    }
+}
+
+window.addEventListener("resize", function(){
+    if(window.innerWidth > 824){
+        this.document.getElementById("listanav").style.display = "none";
+    }
+})
+
--- pagindex/js/script.js
+++ pagindex/js/script.js
@@ -1,22 +1,3 @@
-document.getElementById("botaolista").addEventListener("click", show);
-
-function show(){
-    let listanav = document.getElementById("listanav").style.display;
-    if(listanav === "none")
-    {
-        document.getElementById("listanav").style.display = "block";
-    }
-    else{
-        document.getElementById("listanav").style.display = "none";
-    }
-}
-
-window.addEventListener("resize", function(){
-    if(window.innerWidth > 824){
-        this.document.getElementById("listanav").style.display = "none";
-    }
-})
-
 const controls = document.querySelectorAll('.control')
 
 let currentItem = 0;
